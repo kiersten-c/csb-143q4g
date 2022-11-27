6bd0fc44c9317be3052b6dfb7dc6a451f8b0f282
Code Sandbox <hello@codesandbox.io>
2022-11-27 00:32:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.16b6fbb938a3d6f8d4f8698bb0723eb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a135fe6ce7354e1e1a",
+    "url": "https://kiersten-c.github.io/csb-143q4g/static/css/main.3ed8ef2c.chunk.css"
+  },
+  {
+    "revision": "73a135fe6ce7354e1e1a",
+    "url": "https://kiersten-c.github.io/csb-143q4g/static/js/main.e37afbe9.chunk.js"
+  },
+  {
+    "revision": "33c0c7f09a1a79a11864",
+    "url": "https://kiersten-c.github.io/csb-143q4g/static/js/runtime~main.74e1b99b.js"
+  },
+  {
+    "revision": "012afc425548711902e0",
+    "url": "https://kiersten-c.github.io/csb-143q4g/static/js/2.fe0df1c6.chunk.js"
+  },
+  {
+    "revision": "b489909dc45b25ff22060d5a1d378d07",
+    "url": "https://kiersten-c.github.io/csb-143q4g/index.html"
+  }
+];
